4ff58002def9f5c8e251cf5b22cb06e117994326
Apothe0s1s <118425534+Apothe0s1s@users.noreply.github.com>
2022-12-26 01:20:59 +0300

    HW

---
+++ HW_9/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// int GetNumber(string msg)
+// {
+//     Console.Write(msg);
+//     int Number = Convert.ToInt32(Console.ReadLine());
+//     return Number;
+// }
+
+// string StringRecursion(int number)
+// {
+//    if (number == 1) return number.ToString();
+   
+//    return number + " " + StringRecursion(number - 1);
+// }
+
+// int Number = GetNumber("Введите число: ");
+// Console.Write(StringRecursion(number: Number));
+
+// __________________________________________________________
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.WriteLine("Введите M: ");
+// int numberM = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите N: ");
+// int numberN = int.Parse(Console.ReadLine());
+
+// void GapNumberSum (int numberM, int numberN, int sum)
+// {
+//     if (numberM > numberN) 
+//     {
+//         Console.WriteLine($"Сумма от M до N: {sum}"); 
+//         return;
+//     }
+//     sum = sum + (numberM++);
+//     GapNumberSum(numberM, numberN, sum);
+// }
+
+// GapNumberSum(numberM, numberN, 0);
+
+// _______________________________________________________________
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int functionAkkerman = Ack(m, n);
+
+Console.Write($"A(m,n) = {functionAkkerman} ");
+
+int Ack(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Ack(m - 1, 1);
+  else return Ack(m - 1, Ack(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
